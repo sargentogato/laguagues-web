8d45943e473b47b7f968ca7f22097b22fc0fb709
sargentogato <marlonjvelasquez@gmail.com>
2025-05-12 02:04:12 +0200

    add logo image like a component with an second componente with the text change color of toggle button

---
+++ src/components/logo/LogoImage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo">
+    <svg xmlns="http://www.w3.org/2000/svg" class="logo__world" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lov-name="Globe">
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+      <path d="M2 12h20"></path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" class="logo__users" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lov-name="Users">
+      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
+      <circle cx="9" cy="7" r="4"></circle>
+      <path d="M22 21v-2a4 4 0 0 0-3-3.87"></path>
+      <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .logo__world {
+    color: var(--secondary-color);
+    width: var(--logo-size);
+  }
+
+  .logo__users {
+    position: absolute;
+    color: var(--white);
+    width: 20px;
+    right: 0;
+    bottom: 0;
+  }
+}
+</style>
---
+++ src/components/logo/LogoText.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo__text-box">
+    <span class="logo__text-one">Languages</span>
+    <span class="logo__text-two">Team</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.logo__text-box {
+  display: flex;
+  gap: 5px;
+  font-weight: 600;
+}
+
+
+
+.logo__text-one {
+  color: var(--white);
+}
+
+.logo__text-two {
+  color: var(--secondary-color);
+}
+</style>
