ca5b26c50f2c1187cebf8753ed8406612cc659a5
sargentogato <marlonjvelasquez@gmail.com>
2025-05-13 23:24:36 +0200

    add new section and create new component header

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <NavBar></NavBar>
+    <HeaderPage></HeaderPage>
   </div>
 
 </template>
 
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue';
+import HeaderPage from '@/components/HeaderPage.vue';
 
 </script>
 
