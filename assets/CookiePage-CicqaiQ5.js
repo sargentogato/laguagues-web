38b6f9e8608c5ff516380bd1cd9e2e32a4acd2d5
sargentogato <marlonjvelasquez@gmail.com>
2025-06-18 10:37:26 +0200

    b02e249

---
+++ assets/CookiePage-CicqaiQ5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-ygy2Zft0.js";const t={};function n(r,a){return c(),o("div",null," Política de cookies ")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/PrivacyPage-DHyZWv22.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-ygy2Zft0.js";const n={};function o(t,a){return r(),c("div",null," POLITICA DE PRIVACIDAD ")}const _=e(n,[["render",o]]);export{_ as default};
