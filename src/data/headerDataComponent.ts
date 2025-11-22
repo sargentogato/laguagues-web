156ccb749082836fe296c37ecffab0e2927c67bc
sargentogato <marlonjvelasquez@gmail.com>
2025-11-22 22:14:05 +0100

    HeaderComponent: move data to headerDataComponent

---
+++ src/data/headerDataComponent.ts
@@ -0,0 +1,25 @@
+const title = ['header.title'];
+const subtitle = ['header.subtitle'];
+const messages = ['header.headerMessage1', 'header.headerMessage2', 'header.headerMessage3'];
+
+const images = [
+  {
+    src:     'images/header/language-team-h1.webp',
+    caption: 'header.textImageHeader1',
+  },
+  {
+    src:     'images/header/language-team-h2.webp',
+    caption: 'header.textImageHeader2',
+  },
+  {
+    src:     'images/header/language-team-h3.webp',
+    caption: 'header.textImageHeader3',
+  },
+];
+
+export default {
+  title,
+  subtitle,
+  messages,
+  images,
+};
